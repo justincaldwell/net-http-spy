9b6ab525beb2acc6e6b02c26c05ab593b2b5bb20
Justin Caldwell <jlcaldwell@gmail.com>
2012-12-11 17:51:04 -0700

    move connect entry to actual connection, add timestamp

--- lib/net-http-spy.rb
+++ lib/net-http-spy.rb
@@ -15,40 +15,45 @@ module Net
 
     def initialize(*args, &block)
       self.class.http_logger_options ||= {}
-      defaults =  {:body => false, :trace => false, :limit => -1, :blacklist_terms => []}
+      defaults =  {:body => false, :trace => false, :limit => -1, :blacklist_terms => [], :debug => false}
       self.class.http_logger_options = (self.class.http_logger_options == :default) ? defaults : self.class.http_logger_options
       @logger_options = defaults.merge(self.class.http_logger_options)
       @params_limit = @logger_options[:params_limit] || @logger_options[:limit]
       @body_limit   = @logger_options[:body_limit]   || @logger_options[:limit]
-      @ignore = !@logger_options[:blacklist_terms].detect {|term| /#{term}/ =~ args[0] }.nil?
-
-      self.class.http_logger.info "CONNECT: #{args.inspect}" unless @ignore
 
       old_initialize(*args, &block)
-      @debug_output   = self.class.http_logger unless @ignore
+
+      @debug_output = self.class.http_logger if @logger_options[:debug]
     end
 
 
     def request(*args, &block)
-      @ignore =  true if !@logger_options[:blacklist_terms].detect {|term| /#{term}/ =~ args[0].path }.nil?
-      unless started? || @ignore
+      full_path = @address + args[0].path
+      ignore = !@logger_options[:blacklist_terms].detect {|term| /#{term}/ =~ full_path }.nil?
+
+      if started? || ignore || @logger_options[:debug]
+        result = old_request(*args, &block)
+      else
+        log("CONNECT", [@address, @port].inspect)
         req = args[0].method
-        self.class.http_logger.info "#{req} #{args[0].path}"
-      end
+        log(req, args[0].path)
+        log(req, full_path)
 
-      time_started = Time.now
-      result = old_request(*args, &block)
-      time_taken = Time.now - time_started
-      unless started? || @ignore
+        time_started = Time.now
+        result = old_request(*args, &block)
+        time_taken = Time.now - time_started
 
-        self.class.http_logger.info "PARAMS #{CGI.parse(args[0].body).inspect[0..@params_limit]} " if args[0].body && req != 'CONNECT'
-        self.class.http_logger.info "TRACE: #{caller.reverse}" if @logger_options[:trace]
-        self.class.http_logger.info "BODY: #{(@logger_options[:body] ? result.body : result.class.name)[0..@body_limit]}"
-        self.class.http_logger.info "TIME: #{(time_taken.to_f*1000).round}ms"
+        log("PARAMS", CGI.parse(args[0].body).inspect[0..@params_limit]) if args[0].body
+        log("TRACE",  caller.reverse) if @logger_options[:trace]
+        log("BODY",   (@logger_options[:body] ? result.body : result.class.name)[0..@body_limit])
+        log("TIME",   (time_taken.to_f*1000).round.to_s + 'ms')
       end
       result
     end
 
+    def log(head, message)
+      self.class.http_logger.info("#{Time.now.to_s} #{head}: #{message}") 
+    end
 
   end
 
